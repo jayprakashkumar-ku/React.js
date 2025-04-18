93f3a146daeca0b7dad49d71ff154f60a6550d8a
jayprakashkumar-ku <jayprakashkumar31072004@gmail.com>
2025-04-18 07:37:28 +0530

    First class

---
+++ first-react-app/src/App.jsx
@@ -0,0 +1,19 @@
+import KgButton from "./KgButon";
+import Hello from "./Hello";
+import Random from "./Random";
+function App(){
+
+  return <div>
+    <h1> 
+    This is the Best React Course
+  </h1>
+  {/* <button>Subscribe</button> */}
+  {/* <KgButton></KgButton> */}
+  <Hello></Hello>
+  <Random></Random>
+  <Random></Random>
+  <Random></Random>
+  <Random></Random>
+  </div>
+}
+export default App;
---
+++ first-react-app/src/Hello.jsx
@@ -0,0 +1,20 @@
+function Hello(){
+//variable declaration
+
+  let name = "Munna Kumar";
+let num= 485;
+//Method Declaration in method calling using the function Parenthisis
+
+  let fullName=()=>{
+    return "Jay Prakash Kumar!";
+  };
+  return <h3>
+  {/* Hello This  is the future Speaking! I am Your Son {fullName()} */}
+<p>This is the numbering the content! { num}   Whwt is your full {fullName()}</p>
+
+
+
+
+  </h3>
+}
+export  default Hello;
